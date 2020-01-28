84dfbe55fd52b664b2445574c6ffdf1a09688135
Jamar Torres <Jetorres88@gmail.com>
2020-01-27 23:30:54 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Person from './Person/person';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        < Person/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Person/person.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Person = () => {
+return (
+    <div>
+        <p>Hi, I'm a person</p>
+    </div>
+)
+}
+
+export default Person;
