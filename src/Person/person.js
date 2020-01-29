4d68fbc67ddade6ebd46ea36e8475ebdb5df6377
Jamar Torres <Jetorres88@gmail.com>
2020-01-28 21:30:58 -0500

    added children method to props in Person.js component

--- src/Person/person.js
+++ src/Person/person.js
@@ -5,6 +5,7 @@ const Person = props => {
 return (
     <div>
         <p>Hi I am {props.name} and I am {props.age} years old!</p>
+        {/* props.children is a built in method */}
         <p>{props.children}</p>
     </div>
     )
