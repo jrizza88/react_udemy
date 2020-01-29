12ff25c3473eef8b9612ac84c3e026d09f86cba7
Jamar Torres <Jetorres88@gmail.com>
2020-01-28 21:28:47 -0500

    changed App.js file to include state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Person from './Person/person';
 import './App.css';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Royyale', age: 21},
+      {name: 'Jamar', age: 31},
+      {name: 'Rose', age: 61}
+    ]
+  }
   render() {
     return (
       <div className="App">
+        <button>Switch Name</button>
         < Person name="Random Person" age={Math.floor(Math.random() * 32)}/>
-        < Person name="Jamar" age="31"/>
-        < Person name="Rose" age="61"/>
+        < Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        < Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        < Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
         < Person name="Jordan" age="39"/>
+        < Person name="Jayden" age="11">My hobbies include: Gaming</Person>
       </div>
     );
   }
--- src/Person/person.js
+++ src/Person/person.js
@@ -4,9 +4,8 @@ const Person = props => {
   
 return (
     <div>
-{/* <p>Hi, I'm a person and I am {Math.floor(Math.random() * 32)} years old!</p> */}
-<p>Hi I am {props.name} and I am {props.age} years old!</p>
-<p></p>
+        <p>Hi I am {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
     </div>
     )
 }
